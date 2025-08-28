1270f14e41147b2f87afc429b0a78bbbe066965a
suronjit02 <suronjit02@gmail.com>
2025-08-29 03:55:48 +0600

    heart funstionality complete by js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 
-console.log("hello script")
+// heart icon count
+
+const cardHeart = document.querySelectorAll(".card i.fa-heart");
+
+let navHeart = 0;
+
+for (const heart of cardHeart) {
+
+    heart.addEventListener("click", function () {
+
+        navHeart++;
+        document.getElementById("nav-heart").innerText = navHeart;
+
+    })
+
+}
+
+
